bd199f8f87b8c3097ff0944859e3c7579affb882
Marco Magisano <marco.magisano.7@gmail.com>
2023-05-20 23:39:49 +0300

    Counties by state

--- src/documentation/states.php
+++ src/documentation/states.php
@@ -15,6 +15,44 @@
  *      response="200", 
  *      description="Returns a list of all states with their total population"
  *    ),
+ *    @OA\Response(
+ *      response="400", 
+ *      description="Invalid state ID"
+ *    ),
+ *     @OA\Response(
+ *      response="401", 
+ *      description="Unauthorized. Invalid authorization header"
+ *    ),
+ *     @OA\Response(
+ *      response="500", 
+ *      description="Internal server error"
+ *    )
+ *  )
+**/
+
+/**
+ * @OA\Get(
+ *     path="/states/{state_id}/counties",
+ *     tags={"States"},
+ *     summary="Get counties for a state",
+ *     description="List of all counties with their total population",
+ *     security={{"bearerAuth" : {}}},
+ *     @OA\Parameter(
+ *       name="state_id",
+ *       description="Numeric ID of the state to get",
+ *       in="path",
+ *       @OA\Schema(type="integer"),
+ *       required="true",
+ *       example=1
+ *     ),
+ *     @OA\Response(
+ *      response="200", 
+ *      description="Returns a list of all counties in a state"
+ *    ),
+ *     @OA\Response(
+ *      response="400", 
+ *      description="State ID not valid"
+ *    ),
  *     @OA\Response(
  *      response="401", 
  *      description="Unauthorized. Invalid authorization header"
--- src/routes/states.php
+++ src/routes/states.php
@@ -2,6 +2,7 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use \Respect\Validation\Validator as v;
 
 $app->get('/states', function (Request $request, Response $response) {
 
@@ -31,14 +32,38 @@ $app->get('/states', function (Request $request, Response $response) {
     }
 });
 
-/**
- * @OA\Post(
- *     path="/states",
- *     tags={"States"},
- *     @OA\Response(response="200", 
- * description="Creates a user")
- * )
- */
 
-$app->post('/states', function ($id) {
+$app->get('/states/{state_id}/counties', function (Request $request, Response $response, $args) {
+    try {
+        // There are 51 state_id's = 50 States + DC
+        v::intVal()->between(1,51)->setName("state_id")->assert($args['state_id']);
+
+        $db = $this->get('db');
+        $stmt = $db->prepare('select c.county_id, c."name", c.population  
+        from counties c inner join states s on c.state_id = s.state_id 
+        where s.state_id=:state_id');
+
+        $stmt->bindParam(':state_id', $args['state_id']);
+        $stmt->execute();
+
+        $result = [];
+        $result['status'] = "ok";
+        $result['counties'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $response->getBody()->write(formatOutput($result));
+        return $response->withStatus(200);
+
+    } catch (Respect\Validation\Exceptions\ValidatorException $e) {
+
+        $response->getBody()->write($e->getFullMessage());
+        return $response->withStatus(400);
+    } catch (Exception $e) {
+        $result = [];
+        $result['status'] = "error";
+        $result['description'] = "Internal server error";
+        $response = new \Slim\Psr7\Response(500);
+        $response->getBody()->write(formatOutput($result));
+
+        return $response;
+    }
 });
